a7547a2b6d6987c6ed7b092c2cc77a761f06a668
minoflower31@naver.com <minoflower31@naver.com>
2022-09-20 15:34:07 +0900

    feat: 텍스트 페이지를 조회하는 비즈니스 로직 추가
    splitTextList는 오디오 파일을 포함하고 있으므로 파이썬 내부 서버와 통신하고 나서 추가 구현 필요

--- src/main/java/com/fastcampus/finalproject/service/ProjectService.java
+++ src/main/java/com/fastcampus/finalproject/service/ProjectService.java
@@ -2,8 +2,14 @@ package com.fastcampus.finalproject.service;
 
 import com.fastcampus.finalproject.dto.response.CreateProjectResponse;
 import com.fastcampus.finalproject.dto.response.GetHistoryResponse;
+import com.fastcampus.finalproject.dto.response.GetTextPageResponse;
+import com.fastcampus.finalproject.dto.response.GetTextPageResponse.EnglishDto;
+import com.fastcampus.finalproject.dto.response.GetTextPageResponse.KoreanDto;
 import com.fastcampus.finalproject.entity.Project;
 import com.fastcampus.finalproject.entity.UserBasic;
+import com.fastcampus.finalproject.entity.dummy.DummyVoice;
+import com.fastcampus.finalproject.enums.SexType;
+import com.fastcampus.finalproject.repository.DummyVoiceRepository;
 import com.fastcampus.finalproject.repository.ProjectRepository;
 import com.fastcampus.finalproject.repository.UserRepository;
 import com.fastcampus.finalproject.repository.VideoRepository;
@@ -13,8 +19,14 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.Collections;
 import java.util.List;
+import java.util.NoSuchElementException;
 import java.util.stream.Collectors;
+
+import static com.fastcampus.finalproject.dto.response.GetTextPageResponse.*;
+import static com.fastcampus.finalproject.enums.LanguageType.*;
+import static com.fastcampus.finalproject.enums.SexType.*;
 
 @Service
 @RequiredArgsConstructor
@@ -23,6 +35,7 @@ public class ProjectService {
     private final ProjectRepository projectRepository;
     private final VideoRepository videoRepository;
     private final UserRepository userRepository;
+    private final DummyVoiceRepository dummyVoiceRepository;
 
     @Transactional(readOnly = true)
     public GetHistoryResponse getHistory(Long userUid) {
@@ -51,5 +64,34 @@ public class ProjectService {
 
     private String customizedDateTime(LocalDateTime dateTime) {
         return dateTime.format(DateTimeFormatter.ofPattern("yyyy-MM-dd hh:mm:ss"));
+    }
+
+    @Transactional(readOnly = true)
+    public GetTextPageResponse getTextPageData(Long userId, Long projectId) {
+
+        Project findProject = projectRepository.findByUserUidAndId(userId, projectId)
+                .orElseThrow(NoSuchElementException::new);
+
+        //TODO api 내 api 호출로 해야 splitTextList 구현 가능
+        List<TextDto> splitTextList = Collections.emptyList();
+
+        //dummydata 만드는 함수
+        List<DummyVoice> koreanList = dummyVoiceRepository.findAllByLanguage(KOREAN.getValue());
+        List<DummyVoice> englishList = dummyVoiceRepository.findAllByLanguage(ENGLISH.getValue());
+        List<DummyVoice> chineseList = dummyVoiceRepository.findAllByLanguage(CHINESE.getValue());
+
+        KoreanDto korean = new KoreanDto(getGenderList(koreanList, FEMALE), getGenderList(koreanList, MALE));
+        EnglishDto english = new EnglishDto(getGenderList(englishList, FEMALE), getGenderList(englishList, MALE));
+        ChineseDto chinese = new ChineseDto(getGenderList(chineseList, FEMALE), getGenderList(chineseList, MALE));
+
+        TextPageDummyDto dummyData = new TextPageDummyDto(korean, english, chinese);
+
+        return new GetTextPageResponse(findProject, splitTextList, dummyData);
+    }
+
+    private List<CharacterDto> getGenderList(List<DummyVoice> list, SexType type) {
+        return list.stream().filter(v -> v.getSex().equals(type.getValue()))
+                .map(CharacterDto::new)
+                .collect(Collectors.toList());
     }
 }
