fa361fd5ff18e121521fd6fc81823a7f1d0208d1
minoflower31@naver.com <minoflower31@naver.com>
2022-09-20 15:18:43 +0900

    feat: 프로젝트 및 영상을 조회하는 쿼리 메서드를 각 Repository에 추가
    @Param userUid: 특정 유저로부터 생성된 데이터를 조회하기 위한 파라미터

--- src/main/java/com/fastcampus/finalproject/repository/ProjectRepository.java
+++ src/main/java/com/fastcampus/finalproject/repository/ProjectRepository.java
@@ -2,6 +2,13 @@ package com.fastcampus.finalproject.repository;
 
 import com.fastcampus.finalproject.entity.Project;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface ProjectRepository extends JpaRepository<Project, Long> {
+
+    @Query("select p from Project p where p.user.uid = :userUid order by p.lastModifiedAt desc")
+    List<Project> findAllByUserUid(@Param("userUid") Long userUid);
 }
