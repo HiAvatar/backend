2e422ae1263fe6fac58c10818e7bfeb7f42176ca
minoflower31@naver.com <minoflower31@naver.com>
2022-09-20 15:22:44 +0900

    feat: 프로젝트를 생성하는 비즈니스 로직 추가
    임시 사용자므로 Optional의 데이터를 get()으로 가져옴.
    추후에 일반 회원에 대한 코드를 pull하면 수정할 예정

--- src/main/java/com/fastcampus/finalproject/service/ProjectService.java
+++ src/main/java/com/fastcampus/finalproject/service/ProjectService.java
@@ -1,12 +1,18 @@
 package com.fastcampus.finalproject.service;
 
+import com.fastcampus.finalproject.dto.response.CreateProjectResponse;
 import com.fastcampus.finalproject.dto.response.GetHistoryResponse;
+import com.fastcampus.finalproject.entity.Project;
+import com.fastcampus.finalproject.entity.UserBasic;
 import com.fastcampus.finalproject.repository.ProjectRepository;
+import com.fastcampus.finalproject.repository.UserRepository;
 import com.fastcampus.finalproject.repository.VideoRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -16,6 +22,7 @@ public class ProjectService {
 
     private final ProjectRepository projectRepository;
     private final VideoRepository videoRepository;
+    private final UserRepository userRepository;
 
     @Transactional(readOnly = true)
     public GetHistoryResponse getHistory(Long userUid) {
@@ -31,5 +38,18 @@ public class ProjectService {
                 .collect(Collectors.toList());
 
         return new GetHistoryResponse(projects, videos);
+    }
+
+    @Transactional
+    public CreateProjectResponse create(Long userUid) {
+        UserBasic user = userRepository.findById(userUid).get();
+        Project savedProject = projectRepository.save(new Project(user));
+
+        savedProject.initProjectName(customizedDateTime(savedProject.getCreatedAt()));
+        return new CreateProjectResponse(savedProject.getId(), savedProject.getName());
+    }
+
+    private String customizedDateTime(LocalDateTime dateTime) {
+        return dateTime.format(DateTimeFormatter.ofPattern("yyyy-MM-dd hh:mm:ss"));
     }
 }
