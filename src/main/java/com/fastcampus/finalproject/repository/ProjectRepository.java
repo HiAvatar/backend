aaa12240625ad2adf07005fad740c0f01a2eb2f5
minoflower31@naver.com <minoflower31@naver.com>
2022-09-20 15:28:16 +0900

    feat: 사용자가 특정 프로젝트를 조회하기 위한 쿼리 메서드 추가

--- src/main/java/com/fastcampus/finalproject/repository/ProjectRepository.java
+++ src/main/java/com/fastcampus/finalproject/repository/ProjectRepository.java
@@ -6,9 +6,12 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface ProjectRepository extends JpaRepository<Project, Long> {
 
     @Query("select p from Project p where p.user.uid = :userUid order by p.lastModifiedAt desc")
     List<Project> findAllByUserUid(@Param("userUid") Long userUid);
+
+    Optional<Project> findByUserUidAndId(Long userUid, Long projectId);
 }
