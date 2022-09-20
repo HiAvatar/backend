d688214b60421633c4373d5feff6daf78990e58e
minoflower31@naver.com <minoflower31@naver.com>
2022-09-20 15:16:29 +0900

    feat: Language 기준으로 조회하기 위한 쿼리 메서드 추가

--- src/main/java/com/fastcampus/finalproject/repository/DummyVoiceRepository.java
+++ src/main/java/com/fastcampus/finalproject/repository/DummyVoiceRepository.java
@@ -1,7 +1,11 @@
 package com.fastcampus.finalproject.repository;
 
 import com.fastcampus.finalproject.entity.dummy.DummyVoice;
+import com.fastcampus.finalproject.enums.LanguageType;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface DummyVoiceRepository extends JpaRepository<DummyVoice, String> {
+    List<DummyVoice> findAllByLanguage(String language);
 }
