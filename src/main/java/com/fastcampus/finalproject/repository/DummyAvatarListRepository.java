e4e96f2e0c1988886801c983f9f8ec5c965efd76
minoflower31@naver.com <minoflower31@naver.com>
2022-09-20 19:51:42 +0900

    feat: 아바타 선택 페이지에 대한 비즈니스 로직 구현 및 더미데이터 내 쿼리메서드 추가

--- src/main/java/com/fastcampus/finalproject/repository/DummyAvatarListRepository.java
+++ src/main/java/com/fastcampus/finalproject/repository/DummyAvatarListRepository.java
@@ -3,5 +3,9 @@ package com.fastcampus.finalproject.repository;
 import com.fastcampus.finalproject.entity.dummy.DummyAvatarList;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface DummyAvatarListRepository extends JpaRepository<DummyAvatarList, String> {
+
+    Optional<DummyAvatarList> findByNameEndingWith(String end);
 }
--- src/main/java/com/fastcampus/finalproject/service/ProjectService.java
+++ src/main/java/com/fastcampus/finalproject/service/ProjectService.java
@@ -1,32 +1,37 @@
 package com.fastcampus.finalproject.service;
 
 import com.fastcampus.finalproject.dto.response.CreateProjectResponse;
+import com.fastcampus.finalproject.dto.response.GetAvatarPageResponse;
+import com.fastcampus.finalproject.dto.response.GetAvatarPageResponse.AvatarDivisionDto;
 import com.fastcampus.finalproject.dto.response.GetHistoryResponse;
 import com.fastcampus.finalproject.dto.response.GetTextPageResponse;
-import com.fastcampus.finalproject.dto.response.GetTextPageResponse.EnglishDto;
-import com.fastcampus.finalproject.dto.response.GetTextPageResponse.KoreanDto;
+import com.fastcampus.finalproject.dto.response.GetTextPageResponse.*;
 import com.fastcampus.finalproject.entity.Project;
 import com.fastcampus.finalproject.entity.UserBasic;
+import com.fastcampus.finalproject.entity.dummy.DummyAvatarDivision;
+import com.fastcampus.finalproject.entity.dummy.DummyAvatarList;
 import com.fastcampus.finalproject.entity.dummy.DummyVoice;
 import com.fastcampus.finalproject.enums.SexType;
-import com.fastcampus.finalproject.repository.DummyVoiceRepository;
-import com.fastcampus.finalproject.repository.ProjectRepository;
-import com.fastcampus.finalproject.repository.UserRepository;
-import com.fastcampus.finalproject.repository.VideoRepository;
+import com.fastcampus.finalproject.repository.*;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.NoSuchElementException;
 import java.util.stream.Collectors;
 
+import static com.fastcampus.finalproject.dto.response.GetAvatarPageResponse.*;
+import static com.fastcampus.finalproject.dto.response.GetAvatarPageResponse.AvatarDto;
+import static com.fastcampus.finalproject.dto.response.GetAvatarPageResponse.AvatarPageDummyDto;
 import static com.fastcampus.finalproject.dto.response.GetTextPageResponse.*;
 import static com.fastcampus.finalproject.enums.LanguageType.*;
-import static com.fastcampus.finalproject.enums.SexType.*;
+import static com.fastcampus.finalproject.enums.SexType.FEMALE;
+import static com.fastcampus.finalproject.enums.SexType.MALE;
 
 @Service
 @RequiredArgsConstructor
@@ -36,6 +41,12 @@ public class ProjectService {
     private final VideoRepository videoRepository;
     private final UserRepository userRepository;
     private final DummyVoiceRepository dummyVoiceRepository;
+    private final DummyAvatarListRepository dummyAvatarListRepository;
+    private final DummyAvatarDivisionRepository dummyAvatarDivisionRepository;
+    private final DummyBackgroundRepository dummyBackgroundRepository;
+
+    private static final int START_AVATAR_IDX = 1;
+    private static final int END_AVATAR_IDX = 7;
 
     @Transactional(readOnly = true)
     public GetHistoryResponse getHistory(Long userUid) {
@@ -75,7 +86,6 @@ public class ProjectService {
         //TODO api 내 api 호출로 해야 splitTextList 구현 가능
         List<TextDto> splitTextList = Collections.emptyList();
 
-        //dummydata 만드는 함수
         List<DummyVoice> koreanList = dummyVoiceRepository.findAllByLanguage(KOREAN.getValue());
         List<DummyVoice> englishList = dummyVoiceRepository.findAllByLanguage(ENGLISH.getValue());
         List<DummyVoice> chineseList = dummyVoiceRepository.findAllByLanguage(CHINESE.getValue());
@@ -93,5 +103,44 @@ public class ProjectService {
         return list.stream().filter(v -> v.getSex().equals(type.getValue()))
                 .map(CharacterDto::new)
                 .collect(Collectors.toList());
+    }
+
+    @Transactional(readOnly = true)
+    public GetAvatarPageResponse getAvatarPageData(Long userId, Long projectId) {
+        Project findProject = projectRepository.findByUserUidAndId(userId, projectId)
+                .orElseThrow(NoSuchElementException::new);
+
+        List<AvatarDto> avatarDtoList = getAvatarDtoList();
+        List<BackgroundDto> backgroundDtoList = dummyBackgroundRepository.findAll().stream()
+                .map(BackgroundDto::new)
+                .collect(Collectors.toList());
+
+        AvatarPageDummyDto dummyData = new AvatarPageDummyDto(avatarDtoList, backgroundDtoList);
+
+        return new GetAvatarPageResponse(findProject, dummyData);
+    }
+
+    private List<AvatarDto> getAvatarDtoList() {
+        List<AvatarDto> avatarDtoList = new ArrayList<>();
+
+        for (int i = START_AVATAR_IDX; i <= END_AVATAR_IDX; i++) {
+            List<DummyAvatarDivision> avatarDivisions = dummyAvatarDivisionRepository.findAllByPositionStartingWith(Integer.toString(i));
+            DummyAvatarList dummyAvatar = dummyAvatarListRepository.findByNameEndingWith(Integer.toString(i)).orElseThrow();
+
+            AvatarDto avatar = new AvatarDto(
+                    dummyAvatar.getThumbnail(),
+                    getAvatarDivisions(avatarDivisions, i + "-1-"),
+                    getAvatarDivisions(avatarDivisions, i + "-2-"),
+                    getAvatarDivisions(avatarDivisions, i + "-3-")
+            );
+            avatarDtoList.add(avatar);
+        }
+        return avatarDtoList;
+    }
+
+    private List<AvatarDivisionDto> getAvatarDivisions(List<DummyAvatarDivision> avatarDivisions, String s) {
+        return avatarDivisions.stream()
+                .filter(a -> a.getPosition().contains(s))
+                .map(AvatarDivisionDto::new).collect(Collectors.toList());
     }
 }
