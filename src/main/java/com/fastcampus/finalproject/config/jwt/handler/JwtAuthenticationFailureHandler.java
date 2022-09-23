96dc91218706f1b2532b4524b9908403a7742e81
yongseungLeeDec <yonson.dev@gmail.com>
2022-09-23 22:14:27 +0900

    feat: JwtAuthenticationFailureHandler 추가

---
+++ src/main/java/com/fastcampus/finalproject/config/jwt/handler/JwtAuthenticationFailureHandler.java
@@ -0,0 +1,25 @@
+package com.fastcampus.finalproject.config.jwt.handler;
+
+import com.fastcampus.finalproject.dto.ErrorResponseWrapper;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class JwtAuthenticationFailureHandler implements AuthenticationFailureHandler {
+
+    private static final String ERROR_CODE_LOGIN_FAILURE = "AUTHENTICATION_FAILED";
+    private static final String ERROR_MESSAGE_LOGIN_FAILURE = "아이디가 존재하지 않거나 비밀번호가 올바르지 않습니다";
+
+    @Override
+    public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException, ServletException {
+        ErrorResponseWrapper errorResponse = new ErrorResponseWrapper(ERROR_CODE_LOGIN_FAILURE, ERROR_MESSAGE_LOGIN_FAILURE).unauthorized();
+        String serializedErrorResponse = new ObjectMapper().writeValueAsString(errorResponse);
+        response.getWriter().write(serializedErrorResponse);
+        response.getWriter().flush();
+    }
+}
