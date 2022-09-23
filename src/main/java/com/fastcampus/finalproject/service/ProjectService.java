17faf212301b073974dbd93ac9ec4575554f6aa4
Johyejin-119 <hyejinjo0101@gmail.com>
2022-09-24 00:22:51 +0900

    feat: 오디오 요청 API를 이용한 비즈니스 로직
    - Client로부터 받은 Request를 오디오 요청 API Request로 전달
    - 오디오 요청 API는 audioService.getAudio()에서 실행
    - 오디오 요청 API Response는 Client에게 보낼 Response에 전달

--- src/main/java/com/fastcampus/finalproject/service/ProjectService.java
+++ src/main/java/com/fastcampus/finalproject/service/ProjectService.java
@@ -1,11 +1,9 @@
 package com.fastcampus.finalproject.service;
 
-import com.fastcampus.finalproject.dto.response.CreateProjectResponse;
-import com.fastcampus.finalproject.dto.response.GetAvatarPageResponse;
+import com.fastcampus.finalproject.dto.request.AudioRequest;
+import com.fastcampus.finalproject.dto.request.TextInputRequest;
+import com.fastcampus.finalproject.dto.response.*;
 import com.fastcampus.finalproject.dto.response.GetAvatarPageResponse.AvatarDivisionDto;
-import com.fastcampus.finalproject.dto.response.GetHistoryResponse;
-import com.fastcampus.finalproject.dto.response.GetTextPageResponse;
-import com.fastcampus.finalproject.dto.response.GetTextPageResponse.*;
 import com.fastcampus.finalproject.entity.Project;
 import com.fastcampus.finalproject.entity.UserBasic;
 import com.fastcampus.finalproject.entity.dummy.DummyAvatarDivision;
@@ -14,15 +12,15 @@ import com.fastcampus.finalproject.entity.dummy.DummyVoice;
 import com.fastcampus.finalproject.enums.SexType;
 import com.fastcampus.finalproject.repository.*;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.sql.Array;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.NoSuchElementException;
+import java.util.*;
 import java.util.stream.Collectors;
 
 import static com.fastcampus.finalproject.dto.response.GetAvatarPageResponse.*;
@@ -35,6 +33,7 @@ import static com.fastcampus.finalproject.enums.SexType.MALE;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class ProjectService {
 
     private final ProjectRepository projectRepository;
@@ -44,9 +43,67 @@ public class ProjectService {
     private final DummyAvatarListRepository dummyAvatarListRepository;
     private final DummyAvatarDivisionRepository dummyAvatarDivisionRepository;
     private final DummyBackgroundRepository dummyBackgroundRepository;
+    private final AudioService audioService;
 
     private static final int START_AVATAR_IDX = 1;
     private static final int END_AVATAR_IDX = 7;
+
+
+    /**
+     * 처음 텍스트 팝업창으로 저장
+     * */
+    @Transactional
+    public TextInputResponse getAudio(Long projectId, TextInputRequest texts) throws Exception{
+        Project findProject = projectRepository.findById(projectId).orElseThrow(() -> new RuntimeException("프로젝트가 존재하지 않습니다."));
+
+        AudioResponse audioResponse = new AudioResponse();
+        audioResponse = audioService.getAudio(new AudioRequest(texts.getTexts(), "", "result"));
+
+        TextInputResponse textInputResponse = new TextInputResponse();
+        textInputResponse.setResult(audioResponse.getStatus());
+        textInputResponse.setTotalAudioUrl(audioResponse.getId());
+        findProject.changeTotalAudioURl(audioResponse.getId());
+        return textInputResponse;
+
+/*
+        // Java -> Python 으로  HTTP 요청
+//        AudioRequest audioRequest = new AudioRequest();
+//        log.info("texts.getTexts() : {}", texts.getTexts());
+//        audioRequest.setText(texts.getTexts());
+//        audioRequest.setPath("result");
+//        audioRequest.setNarration("");
+
+
+        // audioRequest 에 texts 를 get 해서 split -> 각 split text 마다 python api 로 요청
+        //String[] splitTexts = audioRequest.getText().split("\\.");
+        List<String> splitTextList = Arrays.asList(texts.getTexts().split("\\."));
+        log.info("splitTextList : {}", splitTextList);
+
+        List<String> audioIdList = new ArrayList<>();
+
+        AudioResponse audioResponse = new AudioResponse();
+        for (String s : splitTextList) {
+            log.info("splitTextList s : {}", s);
+            audioResponse = audioService.getAudio(new AudioRequest(s, "", "result"));
+            audioIdList.add(audioResponse.getId()); // 오디오 이름
+            log.info("audioResponse.getId() : {}", audioResponse.getId());
+        }
+        log.info("audioIdList : {}", audioIdList.toArray()); // 오디오 이름
+
+
+        TextInputResponse textInputResponse = new TextInputResponse();
+        textInputResponse.setResult(audioResponse.getStatus());
+
+        ArrayList<TextInputResponse.Audios> resAudioList = new ArrayList<>();
+        for (int i = 0; i < audioIdList.size(); i++) {
+            resAudioList.add(new TextInputResponse.Audios(Integer.parseInt(audioIdList.get(i)), splitTextList.toString(), audioIdList.toString()));
+        }
+        textInputResponse.setAudios(resAudioList);
+
+        return textInputResponse;
+
+ */
+    }
 
     @Transactional(readOnly = true)
     public GetHistoryResponse getHistory(Long userUid) {
@@ -143,4 +200,5 @@ public class ProjectService {
                 .filter(a -> a.getPosition().contains(s))
                 .map(AvatarDivisionDto::new).collect(Collectors.toList());
     }
+
 }
