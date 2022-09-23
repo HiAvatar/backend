3fccffc4d68043ffda65bb7bc4d73bb88e1d18c7
Johyejin-119 <hyejinjo0101@gmail.com>
2022-09-24 00:16:15 +0900

    feat: 텍스트 페이지 입력창(팝업)에서 완료 버튼 누를 때 동작하는 API

--- src/main/java/com/fastcampus/finalproject/controller/ProjectController.java
+++ src/main/java/com/fastcampus/finalproject/controller/ProjectController.java
@@ -1,27 +1,37 @@
 package com.fastcampus.finalproject.controller;
 
 import com.fastcampus.finalproject.dto.ResponseWrapper;
-import com.fastcampus.finalproject.dto.response.CreateProjectResponse;
-import com.fastcampus.finalproject.dto.response.GetAvatarPageResponse;
-import com.fastcampus.finalproject.dto.response.GetHistoryResponse;
-import com.fastcampus.finalproject.dto.response.GetTextPageResponse;
+import com.fastcampus.finalproject.dto.request.TextInputRequest;
+import com.fastcampus.finalproject.dto.response.*;
 import com.fastcampus.finalproject.entity.UserBasic;
 import com.fastcampus.finalproject.repository.UserRepository;
 import com.fastcampus.finalproject.service.ProjectService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequiredArgsConstructor
+@Slf4j
 public class ProjectController {
 
     private final ProjectService projectService;
     private final UserRepository userRepository;
 
     private static final Long userUid = 1L;
+
+    /**
+     * 텍스트 입력 팝업 창에서 완료 버튼 누를 때
+     * */
+    @PostMapping("/projects/{projectId}/save/script")
+    public ResponseWrapper<TextInputResponse> addTexts(
+            @PathVariable("projectId") Long projectId,
+            @RequestBody TextInputRequest texts) throws Exception {
+        log.info("texts : {}", texts.getTexts());
+        TextInputResponse textInputResponse = projectService.getAudio(projectId, texts);
+        return new ResponseWrapper<>(textInputResponse).ok();
+    }
 
     //init data
     @GetMapping("/login-test")
@@ -53,4 +63,5 @@ public class ProjectController {
     public ResponseWrapper<GetAvatarPageResponse> getAvatarSelectionPage(@PathVariable Long projectId) {
         return new ResponseWrapper<>(projectService.getAvatarPageData(1L, projectId)).ok();
     }
+
 }
