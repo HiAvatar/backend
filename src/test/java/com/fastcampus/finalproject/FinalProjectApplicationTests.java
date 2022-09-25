2675ada45c8f22e1288a34ae90c362e344aafa94
minoflower31@naver.com <minoflower31@naver.com>
2022-09-25 17:26:37 +0900

    chore: jenkins build를 위해 @SpringBootTest 어노테이션 주석 처리

--- src/test/java/com/fastcampus/finalproject/FinalProjectApplicationTests.java
+++ src/test/java/com/fastcampus/finalproject/FinalProjectApplicationTests.java
@@ -3,7 +3,7 @@ package com.fastcampus.finalproject;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class FinalProjectApplicationTests {
 
 	@Test
