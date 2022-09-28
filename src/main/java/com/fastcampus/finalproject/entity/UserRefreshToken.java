d7f9b8e683cc13e682b774dc068137263f53ada0
yongseungLeeDec <yonson.dev@gmail.com>
2022-09-28 22:40:33 +0900

    feat: RefreshToken Redis Entity Dto, Repo 구현

---
+++ src/main/java/com/fastcampus/finalproject/entity/UserRefreshToken.java
@@ -0,0 +1,21 @@
+package com.fastcampus.finalproject.entity;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.redis.core.RedisHash;
+
+@RedisHash(timeToLive = 86400 * 24)
+public class UserRefreshToken {
+
+    private Long uid;
+    private String refreshToken;
+
+    public UserRefreshToken(Long uid, String refreshToken) {
+        this.uid = uid;
+        this.refreshToken = refreshToken;
+    }
+
+    @Id
+    public Long getUid() {
+        return uid;
+    }
+}
