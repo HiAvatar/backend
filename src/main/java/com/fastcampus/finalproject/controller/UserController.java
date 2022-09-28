6361cd81377e1503d16cc7c9658fa178403d4f1d
yongseungLeeDec <yonson.dev@gmail.com>
2022-09-28 22:11:03 +0900

    feat: UserController, UserService 구현

---
+++ src/main/java/com/fastcampus/finalproject/controller/UserController.java
@@ -0,0 +1,97 @@
+package com.fastcampus.finalproject.controller;
+
+import com.fastcampus.finalproject.config.security.AuthUtil;
+import com.fastcampus.finalproject.config.security.jwt.utils.AccessTokenUtility;
+import com.fastcampus.finalproject.config.security.jwt.utils.RefreshTokenUtility;
+import com.fastcampus.finalproject.dto.ResponseWrapper;
+import com.fastcampus.finalproject.dto.request.NewPasswordDto;
+import com.fastcampus.finalproject.dto.request.SignUpInfoDto;
+import com.fastcampus.finalproject.dto.request.auth.IdDto;
+import com.fastcampus.finalproject.dto.response.IdAvailabilityDto;
+import com.fastcampus.finalproject.dto.response.JwtDto;
+import com.fastcampus.finalproject.dto.response.SignUpResultDto;
+import com.fastcampus.finalproject.entity.UserBasic;
+import com.fastcampus.finalproject.entity.UserRefreshToken;
+import com.fastcampus.finalproject.enums.LoginType;
+import com.fastcampus.finalproject.repository.NativeLoginUserRepository;
+import com.fastcampus.finalproject.repository.RefreshTokenRepository;
+import com.fastcampus.finalproject.repository.UserRepository;
+import com.fastcampus.finalproject.service.UserService;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.MalformedJwtException;
+import io.jsonwebtoken.UnsupportedJwtException;
+import io.jsonwebtoken.security.SecurityException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+import java.util.Optional;
+
+@RestController
+public class UserController {
+
+    private final AccessTokenUtility accessTokenUtility;
+    private final RefreshTokenUtility refreshTokenUtility;
+    private final UserService userService;
+    private final RefreshTokenRepository refreshTokenRepository;
+    private final UserRepository userRepository;
+    private final NativeLoginUserRepository nativeLoginUserRepository;
+
+    public UserController(AccessTokenUtility accessTokenUtility, RefreshTokenUtility refreshTokenUtility, UserService userService, RefreshTokenRepository refreshTokenRepository, UserRepository userRepository, NativeLoginUserRepository nativeLoginUserRepository) {
+        this.accessTokenUtility = accessTokenUtility;
+        this.refreshTokenUtility = refreshTokenUtility;
+        this.userService = userService;
+        this.refreshTokenRepository = refreshTokenRepository;
+        this.userRepository = userRepository;
+        this.nativeLoginUserRepository = nativeLoginUserRepository;
+    }
+
+    @PostMapping("/sign-up")
+    public ResponseWrapper<SignUpResultDto> signUp(@RequestBody @Valid SignUpInfoDto signUpInfoDto) {
+        SignUpResultDto result = userService.signUp(signUpInfoDto);
+
+        return new ResponseWrapper<>(result).ok();
+    }
+
+    @PostMapping("/sign-up/check/duplicate-id")
+    public ResponseWrapper<IdAvailabilityDto> checkDuplicateId(@RequestBody @Valid IdDto idDto) {
+        Optional<UserBasic> userBasic = userRepository.findByUserNameAndLoginType(idDto.getId(), LoginType.NATIVE);
+        boolean isIdAvailable = userBasic.isEmpty();
+
+        return new ResponseWrapper<>(new IdAvailabilityDto(isIdAvailable)).ok();
+    }
+
+    @PostMapping("/my-page")
+    public ResponseWrapper changePassword(@Valid NewPasswordDto newPasswordDto) {
+        Long userId = AuthUtil.getCurrentUserUid();
+        userService.changePassword(userId, newPasswordDto);
+
+        return new ResponseWrapper().ok();
+    }
+
+    @PostMapping("/token/refresh")
+    public ResponseWrapper<JwtDto> issueNewTokenPair(@RequestBody String refreshToken) throws ExpiredJwtException, UnsupportedJwtException, MalformedJwtException, SecurityException, IllegalArgumentException {
+        refreshTokenUtility.validateToken(refreshToken); // throws exceptions, exceptions will be handled in controller advice
+
+        Long userUid = refreshTokenUtility.extractSubValueFromPayload(refreshToken);
+        Authentication newAuthToken = new UsernamePasswordAuthenticationToken(userUid, new SimpleGrantedAuthority("ROLE_USER"));
+
+        String newAccessToken = accessTokenUtility.createJsonWebToken(newAuthToken);
+        String newRefreshToken = refreshTokenUtility.createRefreshJsonWebToken(newAuthToken);
+
+        refreshTokenRepository.save(new UserRefreshToken(userUid, newRefreshToken));
+
+        return new ResponseWrapper<>(new JwtDto(newAccessToken, newRefreshToken));
+    }
+
+    @GetMapping("/test/authorization")
+    public String validateAuthorizationProcess() {
+        Long uid = AuthUtil.getCurrentUserUid();
+        return "Authorization, User ID: " + uid.toString();
+    }
+}
---
+++ src/main/java/com/fastcampus/finalproject/service/UserService.java
@@ -0,0 +1,58 @@
+package com.fastcampus.finalproject.service;
+
+import com.fastcampus.finalproject.dto.request.NewPasswordDto;
+import com.fastcampus.finalproject.dto.request.SignUpInfoDto;
+import com.fastcampus.finalproject.dto.response.SignUpResultDto;
+import com.fastcampus.finalproject.entity.NativeLoginUser;
+import com.fastcampus.finalproject.entity.UserBasic;
+import com.fastcampus.finalproject.enums.LoginType;
+import com.fastcampus.finalproject.exception.DuplicateIdException;
+import com.fastcampus.finalproject.repository.NativeLoginUserRepository;
+import com.fastcampus.finalproject.repository.UserRepository;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+    private final UserRepository userRepository;
+    private final NativeLoginUserRepository nativeLoginUserRepository;
+
+    public UserService(BCryptPasswordEncoder bCryptPasswordEncoder, UserRepository userRepository, NativeLoginUserRepository nativeLoginUserRepository) {
+        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+        this.userRepository = userRepository;
+        this.nativeLoginUserRepository = nativeLoginUserRepository;
+    }
+
+    public SignUpResultDto signUp(SignUpInfoDto signUpInfoDto) {
+        String userId = signUpInfoDto.getId();
+        String userPassword = signUpInfoDto.getPassword();
+
+        Optional<UserBasic> userBasic = userRepository.findByUserNameAndLoginType(userId, LoginType.NATIVE);
+
+        if (userBasic.isPresent()) {
+            throw new DuplicateIdException("아이디 '" + userId + "'로 가입한 일반 회원이 이미 존재함");
+        }
+
+        UserBasic signedUpUser = userRepository.save(new UserBasic(userId, LoginType.NATIVE));
+        nativeLoginUserRepository.save(new NativeLoginUser(bCryptPasswordEncoder.encode(userPassword), LocalDateTime.now(), signedUpUser));
+
+        return new SignUpResultDto(signedUpUser.getUserName());
+    }
+
+    public boolean changePassword(Long userUid, NewPasswordDto newPasswordDto) {
+        NativeLoginUser nativeLoginUser = nativeLoginUserRepository.findByUser(new UserBasic(userUid)).orElseThrow();
+
+        nativeLoginUser.changePassword(bCryptPasswordEncoder.encode(newPasswordDto.getNewPassword()));
+        nativeLoginUser.changeLastUpdatedTime(LocalDateTime.now());
+
+        nativeLoginUserRepository.save(nativeLoginUser);
+
+        return true;
+    }
+
+}
