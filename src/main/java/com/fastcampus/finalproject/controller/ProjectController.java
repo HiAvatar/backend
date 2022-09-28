d20d9e28dd197d95e4ac11306196dfcc121d2927
minoflower31@naver.com <minoflower31@naver.com>
2022-09-28 23:31:41 +0900

    feat: AuthUtil로 UserId를 가져와서 적용

--- src/main/java/com/fastcampus/finalproject/controller/ProjectController.java
+++ src/main/java/com/fastcampus/finalproject/controller/ProjectController.java
@@ -1,12 +1,11 @@
 package com.fastcampus.finalproject.controller;
 
+import com.fastcampus.finalproject.config.security.AuthUtil;
 import com.fastcampus.finalproject.dto.ResponseWrapper;
 import com.fastcampus.finalproject.dto.response.CreateProjectResponse;
 import com.fastcampus.finalproject.dto.response.GetAvatarPageResponse;
 import com.fastcampus.finalproject.dto.response.GetHistoryResponse;
 import com.fastcampus.finalproject.dto.response.GetTextPageResponse;
-import com.fastcampus.finalproject.entity.UserBasic;
-import com.fastcampus.finalproject.repository.UserRepository;
 import com.fastcampus.finalproject.service.ProjectService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -19,38 +18,27 @@ import org.springframework.web.bind.annotation.RestController;
 public class ProjectController {
 
     private final ProjectService projectService;
-    private final UserRepository userRepository;
-
-    private static final Long userUid = 1L;
-
-    //init data
-    @GetMapping("/login-test")
-    public ResponseWrapper<Void> login() {
-        userRepository.save(new UserBasic("userA"));
-        return new ResponseWrapper<Void>()
-                .ok();
-    }
 
     @GetMapping("/projects")
     public ResponseWrapper<GetHistoryResponse> getHistory() {
-        return new ResponseWrapper<>(projectService.getHistory(userUid))
+        return new ResponseWrapper<>(projectService.getHistory(AuthUtil.getCurrentUserUid()))
                 .ok();
     }
 
     @PostMapping("/projects")
     public ResponseWrapper<CreateProjectResponse> createProject() {
-        return new ResponseWrapper<>(projectService.create(userUid))
+        return new ResponseWrapper<>(projectService.create(AuthUtil.getCurrentUserUid()))
                 .ok();
     }
 
     @GetMapping("/projects/{projectId}/save")
     public ResponseWrapper<GetTextPageResponse> getTextPage(@PathVariable Long projectId) {
-        return new ResponseWrapper<>(projectService.getTextPageData(1L, projectId))
+        return new ResponseWrapper<>(projectService.getTextPageData(AuthUtil.getCurrentUserUid(), projectId))
                 .ok();
     }
 
     @GetMapping("/projects/{projectId}/avatar")
     public ResponseWrapper<GetAvatarPageResponse> getAvatarSelectionPage(@PathVariable Long projectId) {
-        return new ResponseWrapper<>(projectService.getAvatarPageData(1L, projectId)).ok();
+        return new ResponseWrapper<>(projectService.getAvatarPageData(AuthUtil.getCurrentUserUid(), projectId)).ok();
     }
 }
