7f46256cba44c52a548a84b679fb6b1f6a835880
minoflower31@naver.com <minoflower31@naver.com>
2022-09-29 19:06:47 +0900

    fix: application.yml의 환경 변수를 사용하도록 RedisConfig 클래스에 변수 추가

--- src/main/java/com/fastcampus/finalproject/config/RedisConfig.java
+++ src/main/java/com/fastcampus/finalproject/config/RedisConfig.java
@@ -1,5 +1,6 @@
 package com.fastcampus.finalproject.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
@@ -9,9 +10,15 @@ import org.springframework.data.redis.core.RedisTemplate;
 @Configuration
 public class RedisConfig {
 
+    @Value("${spring.redis.host}")
+    private String host;
+
+    @Value("${spring.redis.port}")
+    private int port;
+
     @Bean
     public JedisConnectionFactory jedisConnectionFactory() {
-        RedisStandaloneConfiguration configuration = new RedisStandaloneConfiguration("localHost", 6379);
+        RedisStandaloneConfiguration configuration = new RedisStandaloneConfiguration(host, port);
         return new JedisConnectionFactory(configuration);
     }
 
