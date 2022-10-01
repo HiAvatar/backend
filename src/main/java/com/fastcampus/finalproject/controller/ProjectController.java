c984d40c9a6b5a65d436c4b3882807c516815a23
Johyejin-119 <hyejinjo0101@gmail.com>
2022-10-01 22:03:43 +0900

    feat: ProjectController에 API 추가
    - 음성 옵션별 전체 음성 URL 생성: /projects/{projectId}/save
    - 문장별 음성 파일 생성: /projects/save/text
    - 음성 파일 업로드: /projects/{projectId}/audio-file
    - 프로젝트 이름 변경: /projects/{projectId}
    - 아바타 옵션별 영상 URL 생성: /projects/{projectId}/avatar

--- src/main/java/com/fastcampus/finalproject/controller/ProjectController.java
+++ src/main/java/com/fastcampus/finalproject/controller/ProjectController.java
@@ -1,15 +1,17 @@
 package com.fastcampus.finalproject.controller;
 
 import com.fastcampus.finalproject.dto.ResponseWrapper;
-import com.fastcampus.finalproject.dto.request.TextInputRequest;
+import com.fastcampus.finalproject.dto.request.*;
 import com.fastcampus.finalproject.dto.response.*;
 import com.fastcampus.finalproject.entity.UserBasic;
 import com.fastcampus.finalproject.repository.UserRepository;
 import com.fastcampus.finalproject.service.ProjectService;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
 
 @RestController
 @RequiredArgsConstructor
@@ -22,7 +24,57 @@ public class ProjectController {
     private static final Long userUid = 1L;
 
     /**
-     * 텍스트 입력 팝업 창에서 완료 버튼 누를 때
+     * 아바타 페이지 - 영상 합성
+     * */
+    @PostMapping("/projects/{projectId}/avatar")
+    public ResponseWrapper<AvatarInfoResponse> addAvatarInfo(
+            @PathVariable Long projectId,
+            @RequestBody AvatarInfoRequest avatarInfoRequest) throws JsonProcessingException {
+        return new ResponseWrapper<>(projectService.addAvatarInfo(projectId, avatarInfoRequest, userUid)).ok();
+    }
+
+    /**
+     * 프로젝트 이름 변경
+     * */
+    @PatchMapping("/projects/{projectId}")
+    public ResponseWrapper<UpdateProjectNameResponse> changeProjectName(
+            @PathVariable Long projectId,
+            @RequestBody UpdateProjectNameRequest projectName) {
+        return new ResponseWrapper<>(projectService.changeProjectName(projectId, projectName)).ok();
+    }
+
+    /**
+     * 로컬에서 업로드된 음성 파일을 DB에 저장
+     * */
+    @PostMapping("/projects/{projectId}/audio-file")
+    public ResponseWrapper<Void> addAudioFile(
+            @PathVariable("projectId") Long projectId,
+            @RequestBody AudioFileUploadRequest audioFile) throws IOException {
+        projectService.addAudioFile(projectId, audioFile);
+        return new ResponseWrapper<Void>().ok();
+    }
+
+    /**
+     * 텍스트 페이지에서 텍스트 하나 수정할 때 -> 음성 합성(File)
+     * */
+    @PostMapping("/projects/save/text")
+    public ResponseWrapper<SentenceInputResponse> addSentence(
+            @RequestBody SentenceInputRequest sentenceInputRequest) throws Exception {
+        return new ResponseWrapper<>(projectService.getAudioFile(sentenceInputRequest)).ok();
+    }
+
+    /**
+     * 텍스트 페이지에서 다음 버튼 또는 전체 듣기 버튼 누를 때 -> 음성 합성(URL)
+     * */
+    @PostMapping("/projects/{projectId}/save")
+    public ResponseWrapper<TotalAudioSyntheticResponse> addTextPageAudioInfo(
+            @PathVariable("projectId") Long projectId,
+            @RequestBody TotalAudioSyntheticRequest totalAudioInfo) throws Exception {
+        return new ResponseWrapper<>(projectService.addAudioInfo(projectId, totalAudioInfo)).ok();
+    }
+
+    /**
+     * 텍스트 입력 팝업 창에서 완료 버튼 누를 때 -> 음성 합성(URL)
      * */
     @PostMapping("/projects/{projectId}/save/script")
     public ResponseWrapper<TextInputResponse> addTexts(
