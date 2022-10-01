3deda2fa69756901a6e93541eb1ba2233e6a46eb
Johyejin-119 <hyejinjo0101@gmail.com>
2022-10-01 21:41:10 +0900

    feat: Python 비디오 요청 API를 사용하기 위한 Response

---
+++ src/main/java/com/fastcampus/finalproject/dto/response/VideoResponse.java
@@ -0,0 +1,11 @@
+package com.fastcampus.finalproject.dto.response;
+
+import lombok.Data;
+
+@Data
+public class VideoResponse {
+    private String status;  // 상태
+    private String id;      // 영상 파일명
+
+
+}
