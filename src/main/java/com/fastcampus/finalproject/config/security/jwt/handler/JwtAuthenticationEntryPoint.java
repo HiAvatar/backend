9a1a07e25aa075dae17dde6f8fac154141166f00
yongseungLeeDec <yonson.dev@gmail.com>
2022-10-04 14:48:27 +0900

    feat: AuthenticationEntryPoint 구현, 관련 설정 수정
    - 사용자가 Bearer 토큰 헤더 없이 요청을 전송할 경우,
      FilterSecurityInterceptor에서 AuthenticationException 발생
    - ExceptionTranslationFilter는 이 예외를 catch하여
      AuthenticationEntryPoint로 예외 처리를 위임
    - JwtAuthenticationEntryPoint는 사용자를 특정 URL(entry point)로 리다이렉트하는 대신, 사용자에게 토큰이 없다는 메시지를 전송

--- src/main/java/com/fastcampus/finalproject/config/security/jwt/handler/JwtAuthenticationEntryPoint.java
+++ src/main/java/com/fastcampus/finalproject/config/security/jwt/handler/JwtAuthenticationEntryPoint.java
@@ -1,5 +1,7 @@
 package com.fastcampus.finalproject.config.security.jwt.handler;
 
+import com.fastcampus.finalproject.dto.ErrorResponseWrapper;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 
@@ -9,8 +11,16 @@ import java.io.IOException;
 
 public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
 
+    private static final String ERROR_CODE_NO_TOKEN = "NO_TOKEN";
+    private static final String ERROR_MESSAGE_NO_TOKEN = "요청에 필요한 토큰이 없습니다.";
+
     @Override
     public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException {
-        response.sendError(HttpServletResponse.SC_UNAUTHORIZED);
+        ErrorResponseWrapper errorResponse = new ErrorResponseWrapper(ERROR_CODE_NO_TOKEN, ERROR_MESSAGE_NO_TOKEN).unauthorized();
+        String serializedErrorResponse = new ObjectMapper().writeValueAsString(errorResponse);
+        response.setContentType("application/json");
+        response.setCharacterEncoding("UTF-8");
+        response.getWriter().write(serializedErrorResponse);
+        response.getWriter().flush();
     }
 }
