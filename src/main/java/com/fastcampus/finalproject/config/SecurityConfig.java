99927942a46beb237e8db56bbf87d687a4cc743d
yongseungLeeDec <yonson.dev@gmail.com>
2022-10-05 21:29:55 +0900

    conf: SecurityConfig- OAuth 관련 빈, 필터 설정 추가
    - SecurityFilterChain에 OAuth 관련 필터 설정 추가
    - AuthenticationManager의 Provider로 OAuthLoginAuthenticationProvider
      추가
    - Oauth2AuthorizationRequestRedirectFilter 빈 설정 메서드 추가
    - Oauth2LoginAuthenticationFilter 빈 설정 메서드 추가
    - ClientRegistrationRepository 빈 설정 메서드 추가
    - ClientRegistration 생성을 위한 getRegistration 메서드 추가
    - CustomOAuth2UserService 빈 설정 메서드 추가
    - DefaultAuthorizationCodeTokenResponseClient 빈 설정 메서드 추가

--- src/main/java/com/fastcampus/finalproject/config/SecurityConfig.java
+++ src/main/java/com/fastcampus/finalproject/config/SecurityConfig.java
@@ -11,16 +11,30 @@ import com.fastcampus.finalproject.config.security.jwt.provider.CustomAuthentica
 import com.fastcampus.finalproject.config.security.jwt.utils.AccessTokenUtility;
 import com.fastcampus.finalproject.config.security.jwt.utils.RefreshTokenUtility;
 import com.fastcampus.finalproject.repository.RefreshTokenRepository;
+import com.fastcampus.finalproject.repository.SocialLoginUserRepository;
+import com.fastcampus.finalproject.repository.UserRepository;
+import com.fastcampus.finalproject.service.auth.CustomOAuth2UserService;
 import com.fastcampus.finalproject.service.auth.CustomUserDetailsService;
 import org.springframework.context.annotation.Bean;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.ProviderManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.config.oauth2.client.CommonOAuth2Provider;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.oauth2.client.InMemoryOAuth2AuthorizedClientService;
+import org.springframework.security.oauth2.client.authentication.OAuth2LoginAuthenticationProvider;
+import org.springframework.security.oauth2.client.endpoint.DefaultAuthorizationCodeTokenResponseClient;
+import org.springframework.security.oauth2.client.registration.ClientRegistration;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.client.registration.InMemoryClientRegistrationRepository;
+import org.springframework.security.oauth2.client.web.OAuth2AuthorizationRequestRedirectFilter;
+import org.springframework.security.oauth2.client.web.OAuth2LoginAuthenticationFilter;
+import org.springframework.security.oauth2.core.AuthorizationGrantType;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
@@ -29,7 +43,10 @@ import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 import javax.servlet.Filter;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
 @Configuration
 public class SecurityConfig {
@@ -37,14 +54,21 @@ public class SecurityConfig {
     private final AccessTokenUtility accessTokenUtility;
     private final RefreshTokenUtility refreshTokenUtility;
     private final RefreshTokenRepository refreshTokenRepository;
-
+    private final UserRepository userRepository;
+    private final SocialLoginUserRepository socialLoginUserRepository;
     private final CustomUserDetailsService userDetailsService;
+    private final Environment env;
+    private static String CLIENT_PROPERTY_KEY = "spring.security.oauth2.client.registration.";
 
-    public SecurityConfig(AccessTokenUtility accessTokenUtility, RefreshTokenUtility refreshTokenUtility, RefreshTokenRepository refreshTokenRepository, CustomUserDetailsService customUserDetailsService) {
+
+    public SecurityConfig(AccessTokenUtility accessTokenUtility, RefreshTokenUtility refreshTokenUtility, RefreshTokenRepository refreshTokenRepository, CustomUserDetailsService customUserDetailsService, UserRepository userRepository, SocialLoginUserRepository socialLoginUserRepository, Environment env) {
         this.accessTokenUtility = accessTokenUtility;
         this.refreshTokenUtility = refreshTokenUtility;
         this.refreshTokenRepository = refreshTokenRepository;
         this.userDetailsService = customUserDetailsService;
+        this.userRepository = userRepository;
+        this.socialLoginUserRepository = socialLoginUserRepository;
+        this.env = env;
     }
 
     @Bean
@@ -70,7 +94,9 @@ public class SecurityConfig {
         httpSecurity
                 .addFilterBefore(jwtAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class)
                 .addFilterBefore(jwtAuthenticationFilter(), JwtAuthorizationFilter.class)
-                .addFilterBefore(jwtExceptionTranslationFilter(), JwtAuthenticationFilter.class);
+                .addFilterBefore(jwtExceptionTranslationFilter(), JwtAuthenticationFilter.class)
+                .addFilterBefore(oAuth2AuthorizationRequestRedirectFilter(), JwtExceptionTranslationFilter.class)
+                .addFilterBefore(oAuth2LoginAuthenticationFilter(), OAuth2LoginAuthenticationFilter.class);
 
         return httpSecurity.build();
     }
@@ -102,6 +128,7 @@ public class SecurityConfig {
     public AuthenticationManager authenticationManager() {
         List<AuthenticationProvider> authProviders = new ArrayList<>();
         authProviders.add(customAuthenticationProvider());
+        authProviders.add(new OAuth2LoginAuthenticationProvider(defaultAuthorizationCodeTokenResponseClient(), customOAuth2UserService()));
 
         return new ProviderManager(authProviders);
     }
@@ -126,7 +153,70 @@ public class SecurityConfig {
         return new BCryptPasswordEncoder();
     }
 
+    // OAuth 2.0 Related Beans
+    @Bean
+    public OAuth2AuthorizationRequestRedirectFilter oAuth2AuthorizationRequestRedirectFilter() {
+        return new OAuth2AuthorizationRequestRedirectFilter(clientRegistrationRepository(), "/oauth2/authorization");
+    }
 
+    @Bean
+    public OAuth2LoginAuthenticationFilter oAuth2LoginAuthenticationFilter() {
+        OAuth2LoginAuthenticationFilter oAuth2LoginAuthenticationFilter = new OAuth2LoginAuthenticationFilter(clientRegistrationRepository(), new InMemoryOAuth2AuthorizedClientService(clientRegistrationRepository()));
+        oAuth2LoginAuthenticationFilter.setAuthenticationManager(authenticationManager());
+        oAuth2LoginAuthenticationFilter.setAuthenticationSuccessHandler(authenticationSuccessHandler());
+        oAuth2LoginAuthenticationFilter.setAuthenticationFailureHandler(authenticationFailureHandler());
+
+        return oAuth2LoginAuthenticationFilter;
+    }
+
+    @Bean
+    public ClientRegistrationRepository clientRegistrationRepository() {
+        List<String> clients = Arrays.asList("google", "kakao");
+        List<ClientRegistration> registrations = clients.stream().map(this::getRegistration).filter(Objects::nonNull).collect(Collectors.toList());
+
+        return new InMemoryClientRegistrationRepository(registrations);
+    }
+
+    private ClientRegistration getRegistration(String client) {
+        String clientId = env.getProperty(CLIENT_PROPERTY_KEY + client + ".client-id");
+
+        if (clientId == null) {
+            return null;
+        }
+
+        String clientSecret = env.getProperty(CLIENT_PROPERTY_KEY + client + ".client-secret");
+
+        if (client.equals("google")) {
+            return CommonOAuth2Provider.GOOGLE.getBuilder(client)
+                    .clientId(clientId).clientSecret(clientSecret).scope("email", "profile").build();
+        }
+
+        if (client.equals("kakao")) {
+            return ClientRegistration.withRegistrationId("kakao")
+                    .clientId(clientId)
+                    .clientSecret(clientSecret)
+                    .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
+                    .redirectUri("http://localhost:8080/login/oauth2/code/kakao")
+                    .authorizationUri("https://kauth.kakao.com/oauth/authorize")
+                    .tokenUri("https://kauth.kakao.com/oauth/token")
+                    .userInfoUri("https://kapi.kakao.com/v2/user/me")
+                    .scope("profile_nickname", "account_email")
+                    .userNameAttributeName("id")
+                    .build();
+        }
+
+        return null;
+    }
+
+    @Bean
+    public CustomOAuth2UserService customOAuth2UserService() {
+        return new CustomOAuth2UserService(userRepository, socialLoginUserRepository);
+    }
+
+    @Bean
+    public DefaultAuthorizationCodeTokenResponseClient defaultAuthorizationCodeTokenResponseClient() {
+        return new DefaultAuthorizationCodeTokenResponseClient();
+    }
 }
 
 
