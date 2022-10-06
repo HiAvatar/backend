9e5b6eca632090aa6ffd8cdaa2ecaa2893aa2fcb
minoflower31@naver.com <minoflower31@naver.com>
2022-10-06 20:58:05 +0900

    feat: flask 서버 내부 오류에 대처하는 ExceptionHandler 추가

--- src/main/java/com/fastcampus/finalproject/controller/advice/ProjectControllerAdvice.java
+++ src/main/java/com/fastcampus/finalproject/controller/advice/ProjectControllerAdvice.java
@@ -6,6 +6,7 @@ import com.fastcampus.finalproject.exception.NoCorrectProjectAccessException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.client.RestClientException;
 
 import java.util.NoSuchElementException;
 
@@ -24,4 +25,14 @@ public class ProjectControllerAdvice {
         return new ErrorResponseWrapper(null, "사용자가 생성한 프로젝트 이외에는 접근할 수 없습니다.")
                 .badRequest();
     }
+
+    /**
+     * flask 서버와 통신하면서 발생하는 예외 처리
+     */
+    @ExceptionHandler
+    public ErrorResponseWrapper test(RestClientException e) {
+        return new ErrorResponseWrapper(e.getMessage(), "flask 서버의 내부 오류로 인해 요청이 실패했습니다.")
+                .internalServerError();
+    }
+
 }
