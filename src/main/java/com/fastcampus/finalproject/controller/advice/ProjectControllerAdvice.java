9d7ec79fdcfede04f4825607261a118097c0c3cb
minoflower31@naver.com <minoflower31@naver.com>
2022-10-06 20:35:15 +0900

    feat: 사용자가 생성한 프로젝트에 다른 사용자의 접근을 차단하는 ExceptionHandler 추가

--- src/main/java/com/fastcampus/finalproject/controller/advice/ProjectControllerAdvice.java
+++ src/main/java/com/fastcampus/finalproject/controller/advice/ProjectControllerAdvice.java
@@ -2,6 +2,7 @@ package com.fastcampus.finalproject.controller.advice;
 
 import com.fastcampus.finalproject.controller.ProjectController;
 import com.fastcampus.finalproject.dto.ErrorResponseWrapper;
+import com.fastcampus.finalproject.exception.NoCorrectProjectAccessException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -14,7 +15,13 @@ public class ProjectControllerAdvice {
 
     @ExceptionHandler
     public ErrorResponseWrapper findProjectExHandle(NoSuchElementException e) {
-        return new ErrorResponseWrapper(null, "프로젝트를 찾을 수 없습니다.").badRequest();
+        return new ErrorResponseWrapper(null, "프로젝트를 찾을 수 없습니다.")
+                .badRequest();
     }
 
+    @ExceptionHandler
+    public ErrorResponseWrapper validateCorrectUser(NoCorrectProjectAccessException e) {
+        return new ErrorResponseWrapper(null, "사용자가 생성한 프로젝트 이외에는 접근할 수 없습니다.")
+                .badRequest();
+    }
 }
