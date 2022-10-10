6404b29beffdfa3512804a936c4c1abfc84fb9d5
minoflower31@naver.com <minoflower31@naver.com>
2022-10-10 17:49:56 +0900

    feat: 두 List의 크기가 맞지 않을 때 발생되는 예외를 ControllerAdvice에서 처리

--- src/main/java/com/fastcampus/finalproject/controller/advice/ProjectControllerAdvice.java
+++ src/main/java/com/fastcampus/finalproject/controller/advice/ProjectControllerAdvice.java
@@ -3,6 +3,7 @@ package com.fastcampus.finalproject.controller.advice;
 import com.fastcampus.finalproject.controller.ProjectController;
 import com.fastcampus.finalproject.dto.ErrorResponseWrapper;
 import com.fastcampus.finalproject.exception.NoCorrectProjectAccessException;
+import com.fastcampus.finalproject.exception.NotSameSizeTwoListsException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -24,6 +25,12 @@ public class ProjectControllerAdvice {
     @ExceptionHandler
     public ErrorResponseWrapper validateCorrectUser(NoCorrectProjectAccessException e) {
         return new ErrorResponseWrapper(null, "사용자가 생성한 프로젝트 이외에는 접근할 수 없습니다.")
+                .badRequest();
+    }
+
+    @ExceptionHandler
+    public ErrorResponseWrapper notSameSizeTwoLists(NotSameSizeTwoListsException e) {
+        return new ErrorResponseWrapper(null, "texts로부터 분리된 splitTextList 데이터가 올바르지 않습니다.")
                 .badRequest();
     }
 
